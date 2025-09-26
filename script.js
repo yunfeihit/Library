919222c8b02fbb08f80946c51c2043a6df495b61
yunfeihit <yunfeihit@gmail.com>
2025-09-26 15:16:50 +0800

    submmit version 1.0

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+//Define the global variable
 const dialog = document.getElementById('add-book-dialog');
 const addBookBtn = document.getElementById('add-book-btn');
-const sumbitBtn = document.querySelector('#submit-btn');
-const form = document.querySelector('#book-form')
+const submitBtn = document.querySelector('#submit-btn');
+const form = document.querySelector('#book-form');
+const cardsContainer = document.querySelector('#cards-container');
+const totalBookCountLog = document.querySelector('#total-book-count');
+const readBookCountLog = document.querySelector('#read-book-count');
+const notReadBookCountLog = document.querySelector('#not-read-book-count');
 
+// Deifine the dialog behavior
 addBookBtn.addEventListener('click', () => {
     dialog.showModal();
 })
 
 
-// Set the Close Behavior of dialog:
-    // if click outside of the dialog, close the dialog
+    // Set the Close Behavior of dialog: if click outside of the dialog, close the dialog
 dialog.addEventListener('click', (event) => {
     // get the DOMrect object of the dialog
     const rect = dialog.getBoundingClientRect()
@@ -28,6 +33,7 @@ dialog.addEventListener('click', (event) => {
 // Set the Data Structure
 const myLibrary = [];
 
+//Object Constructor: Book
 function Book(bookName, author, pages, ifRead) {
     this.bookName = bookName;
     this.author = author;
@@ -35,61 +41,127 @@ function Book(bookName, author, pages, ifRead) {
     this.ifRead = ifRead;
 }
 
+// Function: Create a book object and add it to the Library 
 function addBookToLibrary(bookName, author, pages, ifRead) {
     const oneBook = new Book(bookName, author, pages, ifRead);
     myLibrary.push(oneBook);
 }
 
-
-// Set the Submit button:
-    // prevent the default behavior(submit to the 'action' attribute) and get the input value
-sumbitBtn.addEventListener('click', (event) => {
-    // prevent the default behavior of the form
-    event.preventDefault();
-    // check the form validity by JS(since the default behavior is prevent);
-    if (form.checkValidity()) {
-        const book = document.querySelector('#book-name-input').value;
-        const author = document.querySelector('#book-name-input').value;
-        const pages = document.querySelector('#pages-input').value;
-        const ifRead = document.querySelector('#ifRead-input').value;
-    // if the Validity is checked, create a book object and add it to the Library with 'addBookToLibrary' function
-        addBookToLibrary(book, author, pages, ifRead);
-    // show the book cars with 'showCards' function
-    showCards(myLibrary);
-    }
-    else {
-        form.reportValidity();
-    }
-})
-
-
-// Function: Show the Books by Cards
+// Function: Show the Books by Cards 
 function showCards(array) {
+    // Clear the card container first(list all the cards after that)
+    cardsContainer.innerHTML = '';
+    // Create all book cards(for everey item in the Library array)
     array.forEach((item) => {
-        // Create all the element
+        // Create all the element of the card
         const card = document.createElement('div');
         const bookName = document.createElement('h2');
         const author = document.createElement('h2');
         const pages = document.createElement('h2');
+        const ifRead = document.createElement('h2');
+        const deleteCardBtn = document.createElement('button');
+
         // Set the class name of the created element
         card.className = 'card';
         bookName.className = 'book-name';
         author.className = 'author';
         pages.className = 'pages';
-        // add text content to the created element
-        bookName.textContent = item.bookName;
+        ifRead.className = 'ifRead';
+        deleteCardBtn.className = 'deleteCardBtn';
+
+        // Add text content to the created element
+        bookName.textContent = `'${item.bookName}'`;
         author.textContent = item.author;
-        pages.textContent = '{item.pages}:Pages';
-        // make them a smal DOM tree
+        pages.textContent = `${item.pages} Pages`;
+        if (item.ifRead) {
+            ifRead.textContent = `Read`;
+            ifRead.style.color = 'green';
+        } else {
+            ifRead.textContent = `Not Read`
+            ifRead.style.color = 'red';
+        };
+        deleteCardBtn.textContent = 'Delete';
+
+        // Set operation code of deleteCardBtn
+        deleteCardBtn.addEventListener('click', ()=> {
+        // IMPORTENT!!!: it should also delete the item in the array(myLibrary)
+            const deleteBookIndex = array.findIndex( item => `'${item.bookName}'` === bookName.textContent);
+            array.splice(deleteBookIndex, 1);
+        // Update: run 'showCards' function
+            showCards(myLibrary);
+        // Update: run 'updateLibraryLog' function
+            updateLibraryLog();
+        })
+
+        // Make items a smal DOM tree
         card.appendChild(bookName);
         card.appendChild(author);
         card.appendChild(pages);
+        card.appendChild(ifRead);
+        card.appendChild(deleteCardBtn);
+
+        // Put (every)card inside the container
+        cardsContainer.appendChild(card);   
     })
+    console.log(myLibrary);
+}
+
+// Function: undate the Library log
+function updateLibraryLog() {
+    totalBookCountLog.textContent = myLibrary.length;
+    readBookCountLog.textContent = myLibrary.filter( item => item.ifRead === true ).length;
+    notReadBookCountLog.textContent = myLibrary.filter( item => item.ifRead === false).length;
+}
+
+// Function: check if the book is already exist
+function checkIfBookExist (bookName) {
+    const ifBookExist = myLibrary.some( book => book.bookName === bookName);
+    const existAlert = document.querySelector('#exist-alert');
+    existAlert.style.display = 'none';
+    if (ifBookExist) {        
+        existAlert.style.display = 'block';
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
+// Set the Submit button:
+    // 1. prevent the default behavior(submit to the 'action' attribute)
+    // 2. check the validity (by 'form.checkValidity()', which will check the self-set validity in the form html)
+    // 3. check if the book(name) is already exist
+    // 4. get the input value
+    // 5. add the book to the library
+    // 6. show all the book of the library by Cards
+    // 7. update the Library log
+    // 8. close the dialog
+submitBtn.addEventListener('click', (event) => {
+    // prevent the default behavior of the form(and redefin it)
+    event.preventDefault();
+    // check the form validity by JS(since the default behavior is prevent):
+    if (form.checkValidity()) {
+        const book = document.querySelector('#book-name-input').value;
+        const author = document.querySelector('#author-input').value;
+        const pages = document.querySelector('#pages-input').value;
+        const ifRead = document.querySelector('#ifRead-input').checked;
+    // check if the book(name) is already exist
+        if (checkIfBookExist(book)) {
+            return
+        };
+    // if the Validity is checked, run 'addBookToLibrary' function
+        addBookToLibrary(book, author, pages, ifRead);
+    // run 'showCards' function
+        showCards(myLibrary);
+    // run updateLibraryLog function
+        updateLibraryLog();
+    // close the dialog after that
+        dialog.close();
+    }
+    else {
+        form.reportValidity();
+    }
 
-
-
+})
 
 
